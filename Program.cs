af7ea73ebf95bfcb58c060aac3b0e3bd9c882972
sukanya2712 <sukanyanaik511@gmail.com>
2023-06-08 15:01:36 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            primenumber obj1 = new primenumber();
+            obj1.prime();
+        }
+    }
+}
---
+++ primenumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class primenumber
+    {
+        public void prime()
+        {
+            Boolean flag = true;
+            Console.WriteLine("Enter a number to check weather it is prime or not:- ");
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= a / 2; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("is not a prime number");
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                Console.WriteLine("it is prime");
+            }
+        }
+    }
+}
