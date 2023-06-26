a986bc79d054d2cb1c19776958963f838ae73110
sukanya2712 <sukanyanaik511@gmail.com>
2023-06-26 20:25:52 +0530

    added

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
             //primenumber obj1 = new primenumber();
             //obj1.prime();
             //LeapYear obj2 = new LeapYear();
